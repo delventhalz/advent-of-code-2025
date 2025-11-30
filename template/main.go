fd0dccc57549952981564f46bfa3a40b37024b6f
Zac Delventhal <contrib@zacis.me>
2025-11-30 15:15:41 -0600

    Add template Go solver
    Going to eventually build a generator to make this automatically,
    but let's manually solve with it a few nights first.

---
+++ template/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func partOne(input string) string {
+	return ""
+}
+
+func partTwo(input string) string {
+	return ""
+}
+
+func main() {
+	inputBytes, err := os.ReadFile("input.txt")
+
+	if err != nil {
+		fmt.Println("Error reading input.txt:")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	input := string(inputBytes)
+	fmt.Printf("Part One Solution: %s\n", partOne(input))
+	fmt.Printf("Part Two Solution: %s\n", partTwo(input))
+}
